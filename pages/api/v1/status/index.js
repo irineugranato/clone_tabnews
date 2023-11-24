13d781cbc56135f6f9e30dc217f865303328aaf0
Irineu Granato <irineugranato@gmail.com>
2023-11-24 15:11:03 +0000

    add `database.js`and `.env`files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('select 1 + 3;');
-  console.log(result.rows);
+  const result = await database.query('SELECT 1 + 3 as soma;');
+  console.log(result);
   response.status(200).json({chave: "Resposta da API do curso .dev ok"});
 };
 export default status;

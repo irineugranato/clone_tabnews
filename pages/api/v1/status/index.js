723821fea3043988b33d5a28c6a235fcf3524945
Irineu Granato <irineugranato@gmail.com>
2023-11-30 22:10:24 +0000

    ajuste da soma

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 3 as soma;');
+  const result = await database.query('SELECT 3 + 3 as soma;');
   console.log(result);
   response.status(200).json({chave: "Resposta da API do curso .dev ok"});
 };

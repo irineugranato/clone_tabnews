2f72b261f16bbeea23c0e1e709e7e48fa0a62727
Irineu Granato <irineugranato@gmail.com>
2023-12-01 13:57:15 +0000

    configuração do arquivo de `get.test.js` e api `index.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,10 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 3 + 3 as soma;');
-  console.log(result);
-  response.status(200).json({chave: "Resposta da API do curso .dev ok"});
-};
-export default status;
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
+}
+  export default status;
+
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,16 @@
 
 
 test("Get para /api/v1/status deve retornar 200", async () => {
-  const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+  //const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+  const response = await fetch("https://power2b.io/api/v1/status");
+  
   expect(response.status).toBe(200);
+
+ const responseBody = await response.json();
+ expect(responseBody.updated_at).toBeDefined();
+ const parserupdatedAt = new Date(responseBody.updated_at).toISOString();
+ 
+ console.log(parserupdatedAt);
+ expect (responseBody.updated_at).toEqual(parserupdatedAt);
+
 });
